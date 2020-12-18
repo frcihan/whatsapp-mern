a45a41b7a7697cc0278778a631fc6bb200bfb4d6
ferruhcihan <ferruhcihan20@gmail.com>
2020-12-19 00:24:50 +0300

    project updated

--- src/App.js
+++ src/App.js
@@ -5,9 +5,10 @@ import { Chat } from "./components/Chat";
 function App() {
   return (
     <div className="app">
-      <h1>Hello Whatsapp</h1>
-      <Sidebar />
-      <Chat />
+      <div className="app__body">
+        <Sidebar />
+        <Chat />
+      </div>
     </div>
   );
 }
