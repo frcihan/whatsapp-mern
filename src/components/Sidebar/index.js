3105a87f0dc0091b61f722bd00b0f33b86ad82bb
ferruhcihan <ferruhcihan20@gmail.com>
2020-12-18 20:36:09 +0300

    sidebar index updated

---
+++ src/components/Sidebar/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { StyledSidebar, } from "./Sidebar.style"
+
+export const Sidebar = () => {
+    return (
+        <StyledSidebar>
+            <h1>Styled Sidebar</h1>
+            
+        </StyledSidebar>
+    )
+}
