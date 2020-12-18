ac5d6656fd8ead76aef9493f4d6ac083efe9ccb1
ferruhcihan <ferruhcihan20@gmail.com>
2020-12-18 20:35:56 +0300

    chat index updated

---
+++ src/components/Chat/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { StyledChat, } from "./Chat.style"
+
+export const Chat = () => {
+    return (
+        <StyledChat>
+            <h1>Styled Chat</h1>
+            
+        </StyledChat>
+    )
+}
