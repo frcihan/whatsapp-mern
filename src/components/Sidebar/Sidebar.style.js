03a8a30e345b95bdf8c4520af74a01bbeab7a8df
ferruhcihan <ferruhcihan20@gmail.com>
2020-12-19 15:16:16 +0300

    Sidebar component updated

--- src/components/Sidebar/Sidebar.style.js
+++ src/components/Sidebar/Sidebar.style.js
@@ -1,6 +1,56 @@
 import styled, { css } from "styled-components";
 
 export const StyledSidebar = styled.div`
-    margin: 2rem;
-    background-color: turquoise;
+    display: flex;
+    flex-direction: column;
+    flex: 0.35;
+`
+
+export const StyledSidebarHeader = styled.div`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 15px;
+    border-right: 1px solid lightgray;
+`
+
+export const StyledSidebarHeaderRight = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    min-width: 10vw;
+
+    & .MuiSvgIcon-root {
+        margin-right: 0.5vw;
+        font-size: 24px !important;
+    }
+`
+
+export const StyledSidebarSearch = styled.div`
+    display: flex;
+    align-items: center;
+    background-color: #f6f6f6;
+    height: 39px;
+    padding: 10px;
+`
+export const StyledSidebarSearchContainer = styled.div`
+    display: flex;
+    align-items: center;
+    background-color: #fff;
+    width: 100%;
+    height: 35px;    
+    border-radius: 20px;
+
+    & .MuiSvgIcon-root {
+        color: gray;
+        padding: 10px;
+    }
+`
+export const StyledSidebarSearchInput = styled.input`
+    outline: none;
+    border: none;
+`
+
+export const StyledSidebarChatsBox = styled.div`
+
 `
--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,11 +1,44 @@
 import React from 'react'
-import { StyledSidebar, } from "./Sidebar.style"
+import { 
+    StyledSidebar, 
+    StyledSidebarHeader, 
+    StyledSidebarHeaderRight, 
+    StyledSidebarSearch, 
+    StyledSidebarSearchContainer, 
+    StyledSidebarSearchInput, 
+} from "./Sidebar.style"
+import { Chat, DonutLarge, MoreVert, SearchOutlined } from '@material-ui/icons';
+import { Avatar, IconButton } from "@material-ui/core";
+import { SidebarChats } from "../SidebarChats"
 
 export const Sidebar = () => {
     return (
         <StyledSidebar>
-            <h1>Styled Sidebar</h1>
-            
+            <StyledSidebarHeader>
+                <Avatar src="https://thispersondoesnotexist.com/image" />
+                <StyledSidebarHeaderRight>
+                    <IconButton>
+                        <DonutLarge/>
+                    </IconButton>
+                    <IconButton>
+                        <Chat/>
+                    </IconButton>
+                    <IconButton>
+                        <MoreVert/>
+                    </IconButton>
+                </StyledSidebarHeaderRight>
+            </StyledSidebarHeader>
+            <StyledSidebarSearch>
+                <StyledSidebarSearchContainer>
+                    <SearchOutlined/>
+                    <StyledSidebarSearchInput
+                        placeholder="Search or start new chat"
+                        type="text"
+                    />
+                </StyledSidebarSearchContainer>
+            </StyledSidebarSearch>
+            <SidebarChats/>
+
         </StyledSidebar>
     )
 }
