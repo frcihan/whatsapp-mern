f190377674e268c067542830ea1447d03874d934
ferruhcihan <ferruhcihan20@gmail.com>
2020-12-19 15:16:02 +0300

    Chat component updated

--- src/components/Chat/Chat.style.js
+++ src/components/Chat/Chat.style.js
@@ -1,6 +1,35 @@
 import styled, { css } from "styled-components";
 
 export const StyledChat = styled.div`
-    margin: 2rem;
-    background-color: yellowgreen;
+    display: flex;
+    flex-direction: column;
+    flex: 0.65;
+`
+
+export const StyledChatHeader = styled.div`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 15px;
+    border-bottom: 1px solid lightgray;
+`
+
+export const StyledChatHeaderInfo = styled.div`
+    flex: 1;
+    display: flex;
+    flex-direction: column;
+    padding-left: 20px;
+`
+
+export const StyledChatHeaderInfoTitle = styled.h3`
+    margin-bottom: 3px;
+    font-weight: 500;
+`
+
+export const StyledChatHeaderInfoText = styled.p`
+    color: gray;
+`
+
+export const StyledChatHeaderRight = styled.div`
+    
 `
--- src/components/Chat/index.js
+++ src/components/Chat/index.js
@@ -1,10 +1,42 @@
 import React from 'react'
-import { StyledChat, } from "./Chat.style"
+import { 
+    StyledChat, 
+    StyledChatHeader,
+    StyledChatHeaderInfo,
+    StyledChatHeaderInfoTitle,
+    StyledChatHeaderInfoText,
+    StyledChatHeaderRight,
+} from "./Chat.style"
+import { Avatar, IconButton } from "@material-ui/core";
+import { AttachFile, MoreVert, SearchOutlined } from '@material-ui/icons';
 
 export const Chat = () => {
     return (
         <StyledChat>
-            <h1>Styled Chat</h1>
+            <StyledChatHeader>
+                <Avatar 
+                    src="https://images.generated.photos/xsjVzuwLEC5fCshsdwllZJ2e9b5qK50BnRsDHf1AXzU/rs:fit:256:256/Z3M6Ly9nZW5lcmF0/ZWQtcGhvdG9zL3Yz/XzAwOTQ1MDguanBn.jpg"
+                />
+                <StyledChatHeaderInfo>
+                    <StyledChatHeaderInfoTitle>
+                        Room name
+                    </StyledChatHeaderInfoTitle>
+                    <StyledChatHeaderInfoText>
+                        Last seen at...
+                    </StyledChatHeaderInfoText>
+                </StyledChatHeaderInfo>
+                <StyledChatHeaderRight>
+                    <IconButton>
+                        <SearchOutlined/>
+                    </IconButton>
+                    <IconButton>
+                        <AttachFile/>
+                    </IconButton>
+                    <IconButton>
+                        <MoreVert/>
+                    </IconButton>
+                </StyledChatHeaderRight>
+            </StyledChatHeader>
             
         </StyledChat>
     )
